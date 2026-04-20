99d8c3174fed90d2b2310d5c5a55a3420c1357f1
PhilipOndieki <philipbarongo30@gmail.com>
2026-04-20 06:41:14 +0300

    server folder

--- src/middleware/roles.js
+++
@@ -1,9 +0,0 @@
-// factory so we can do requireRole('admin') or requireRole('agent') inline on routes
-const requireRole = (role) => (req, res, next) => {
-  if (req.user?.role !== role) {
-    return res.status(403).json({ success: false, message: 'Access denied' });
-  }
-  next();
-};
-
-module.exports = { requireRole };
